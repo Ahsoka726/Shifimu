87d8d297c5e5548bce21a23f0972418fa917a930
NawelleBedr <bedrouni.nawelle@gmail.com>
2022-12-05 09:37:11 +0100

    avancement 5/12/2022

--- public/assets/js/shifumi.js
+++ public/assets/js/shifumi.js
@@ -1,6 +1,5 @@
 let btnReset = document.getElementById('buttonReset'); //déclaration du bouton reset via l'id.
-// let btnStart = document.getElementById('buttonStart'); //déclaration du bouton start via l'id.
-// let timer = document.getElementById('time'); //déclaration du timer via l'id.
+
 
 let scorePlayer = document.getElementById('scorePlayer'); //déclaration du compteur de point du joueur via l'id.
 let scoreIa = document.getElementById('scoreIa'); //déclaration du compteur de point de l'ordinateur via l'id.
@@ -16,16 +15,12 @@ let rock = document.getElementById('imgRock');//déclaration du button pierre  d
 // let paperIa = document.getElementById('imgPaperIa');//déclaration du button feuille de l'ordinateur via l'id.
 // let rockIa = document.getElementById('imgRockIa');//déclaration du button pierre  de l'ordinateur via l'id.
 
-// let scissorIa = 0;
-// let paperIa = 0;
-// let rockIa = 0;
-
 //Choix Ordinateur
 function randomIaChoose(min, max){
     return Math.floor(Math.random()*(max-min))+min;
 }
 
-function scissorchoose(){
+function scissorChoose(){
     let random = randomIaChoose(1,4);
     let ciseauxValue = scissor.value;
     if (ciseauxValue == random ) {
@@ -39,7 +34,7 @@ function scissorchoose(){
     }
 }
 
-function paperchoose(){
+function paperChoose(){
     let random = randomIaChoose(1,4);
     let feuilleValue = paper.value;
     if (feuilleValue == random ) {
@@ -67,8 +62,8 @@ function rockChoose () {
     }
 }
 
-scissor.addEventListener('click', scissorchoose)
-paper.addEventListener('click', paperchoose)
+scissor.addEventListener('click', scissorChoose)
+paper.addEventListener('click', paperChoose)
 rock.addEventListener('click', rockChoose )
 
 
@@ -88,59 +83,3 @@ btnReset.addEventListener('click', () =>{
     scorePlayer.textContent = 0;
     message.innerHTML ="";
 })
-
-    // if(random == 1){
-    //    let scissorIa = random;
-    //     }else if(random == 2){
-    //    let paperIa = random;  
-    // }else{
-    //    let rockIa = random;
-    // }
-
-    // console.log(random);
-
-
-
-// console.log(random); 
-
-// Choix Player
-// function playerVsIa(player, ia) {
-//     // scissorPlayer = Number(scissorPlayer.value);
-//     // paperPlayer =  Number(paperPlayer.value);
-//     // rockPlayer =  Number(rockPlayer.value);
-
-//     // Player choose scissors
-//     if(player == scissorPlayer && ia== 1) {
-//         message.innerHTML = "Vous êtes à égalité";
-//         }else if(player == scissorPlayer && ia== 2){
-//         message.innerHTML = "Vous avez gagné cette manche !";
-//         return victoryPlayer();
-//     }else {
-//         message.innerHTML = "Vous avez perdu cette manche !";
-//         return victoryIa();
-//     }
-
-//     //PLayer choose paper
-//     if(player == paperPlayer && ia == 1){
-//         message.innerHTML = "Vous avez perdu cette manche !";
-//         return victoryIa()
-//         }else if(player == paperPlayer && ia== 2){
-//         message.innerHTML = "Vous êtes à égalité";
-//     }else{
-//         message.innerHTML = "Vous avez gagné cette manche !";
-//         return victoryPlayer();
-//     }
-
-//     //Player choose rock
-//     if(player == rockPlayer && ia == 1) {
-//         message.innerHTML = "Vous avez gagné cette manche !";
-//         return victoryPlayer();
-//     } else if(player == rockPlayer && ia == 2){
-//         message.innerHTML = "Vous avez perdu cette manche !";
-//         return victoryIa()
-//     }else{
-//         message.innerHTML = "Vous êtes à égalité";
-//     }
-// }
-
-// let ia = random;//déclaration de la variable ia pour random.
